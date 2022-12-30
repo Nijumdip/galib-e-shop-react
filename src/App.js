dc24da6deff57aae312ee21ecd2098c9fbf33ea3
Nijumdip <nijumdip019911@gmail.com>
2022-12-30 18:44:04 +0600

    added banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./Components/Footer";
+import Home from "./Components/Home/Home";
 import Navigation from "./Components/Navigation";
 
 function App() {
     return (
         <div className="App">
         <Navigation />
+        <Home />
         
         {/* <Footer /> */}
         </div>
---
+++ src/Components/Home/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <>
+            <h1
+                href="/"
+                className=" normal-case font-bold text-3xl text-white absolute z-10 right-2/4 bottom-2/4"
+            >
+                Galib E Shop
+            </h1>
+            <div style={{ position: 'relative',  height: '470px' }} className="w-full" >
+                <img
+                    src="https://assets-us-01.kc-usercontent.com/0542d611-b6d8-4320-a4f4-35ac5cbf43a6/48e65be9-308d-4748-8e4c-0acef4c98f11/commercial-crime-insurance-header.jpg?w=930&h=335&fit=crop"
+                    alt="laptop"
+                    className='w-full'
+                />
+            </div>
+        </>
+    );
+};
+
+export default Banner;
