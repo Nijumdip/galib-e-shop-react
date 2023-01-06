7e53d567e95274f633380a11317edc63949dbf98
Nijumdip <nijumdip019911@gmail.com>
2023-01-06 15:05:54 +0600

    added hero setion

--- src/Components/Home/HeroLaptop.js
+++ src/Components/Home/HeroLaptop.js
@@ -3,36 +3,37 @@ import React from "react";
 const HeroLaptop = () => {
     return (
         <>
-            <div className="card lg:card-side bg-base-100 shadow-xl mb-10">
-                <figure>
-                    <img src="https://placeimg.com/400/400/arch" alt="Album" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">New album is released!</h2>
-                    <p>Click the button to listen on Spotiwhy app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Listen</button>
-                    </div>
+            <div className="flex flex-row  mb-10">
+                <div>
+                    <img
+                        src="https://placeimg.com/400/400/arch"
+                        alt="Album" />
+                </div>
+                <div className="flex flex-col">
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                        exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
 
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img
-                        src="https://placeimg.com/260/400/arch"
-                        className="max-w-sm rounded-lg shadow-2xl"
-                        alt=""
-                    />
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-                            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-                            id nisi.
-                        </p>
-                        <button className="btn btn-primary">Get Started</button>
-                    </div>
+            <div className="card lg:card-side bg-base-100  mb-10">
+                
+                <div>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                        exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
                 </div>
+                <figure>
+                    <img
+                        src="https://placeimg.com/400/400/arch"
+                        alt="Album" />
+                </figure>
             </div>
         </>
     );
