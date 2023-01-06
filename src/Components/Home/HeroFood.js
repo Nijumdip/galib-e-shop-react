da4957b3ab00c95046c608cf808e96ce22e897ff
Nijumdip <nijumdip019911@gmail.com>
2023-01-06 17:24:04 +0600

    done hero laptop

---
+++ src/Components/Home/HeroFood.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroFood = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeroFood;
--- src/Components/Home/HeroLaptop.js
+++ src/Components/Home/HeroLaptop.js
@@ -3,24 +3,40 @@ import React from "react";
 const HeroLaptop = () => {
     return (
         <>
-            <div className="flex flex-row  mb-10">
-                <div>
+            <div className="card flex sm:flex-row flex-col bg-base-100  mb-10">
+                <div className="text-start sm:w-1/2 h-96">
                     <img
+                        className="w-full "
                         src="https://placeimg.com/400/400/arch"
-                        alt="Album" />
+                        alt="Album"
+                    />
                 </div>
-                <div className="flex flex-col">
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-                        exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+                <div className="text-start mt-10 sm:ml-10 px-5 sm:w-1/2">
+                    <h1 className="text-5xl font-bold">LAPTOP</h1>
+                    <p className="py-2">
+                        A fast-loading e-commerce website with quality content increases organic
+                        traffic and gives you a stream of targeted visitors. You get a fully
+                        optimized e-commerce website that takes top positions of search engine
+                        results and boosts both your brand awareness and sales.Provide flexible,
+                        personalized website search and navigation in your turn-key digital store to
+                        ensure a delightful customer experience . We will support you at every step
+                        of e-commerce website development
+                    </p>
+
+                    <p className="py-3">
+                        A fast-loading e-commerce website with quality content increases organic
+                        traffic and gives you a stream of targeted visitors. You get a fully
+                        optimized e-commerce website that takes top positions of search engine
+                        results and boosts both your brand awareness and sales.Provide flexible,
+                        personalized website search and navigation in your turn-key digital store to
+                        ensure a delightful customer experience . We will support you at every step
+                        of e-commerce website development
                     </p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
 
-            <div className="card lg:card-side bg-base-100  mb-10">
-                
+            <div className="card flex flex-row bg-base-100  mb-10">
                 <div>
                     <h1 className="text-5xl font-bold">Box Office News!</h1>
                     <p className="py-6">
@@ -29,11 +45,9 @@ const HeroLaptop = () => {
                     </p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
-                <figure>
-                    <img
-                        src="https://placeimg.com/400/400/arch"
-                        alt="Album" />
-                </figure>
+                <div>
+                    <img src="https://placeimg.com/400/400/arch" alt="Album" />
+                </div>
             </div>
         </>
     );
