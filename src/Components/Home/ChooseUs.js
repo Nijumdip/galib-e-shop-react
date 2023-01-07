7bc4671a87816dc3b4d1af4ab5d3fec945d70de5
Nijumdip <nijumdip019911@gmail.com>
2023-01-07 13:21:07 +0600

    added choose us page

--- src/Components/Home/ChooseUs.js
+++ src/Components/Home/ChooseUs.js
@@ -3,122 +3,17 @@ import React from "react";
 const ChooseUs = () => {
     return (
         <>
-            <div className="mx-auto container py-20 px-6">
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                    <div className="rounded">
-                        <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                            <div>
-                                <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                                    What does success as a UX designer look like and how to get
-                                    there systematically
-                                </h3>
-                            </div>
-                            <div>
-                                
-                            </div>
-                        </div>
+            <div className="flex flex-col justify-center items-center ">
+                <h2 style={{color:"#00A9E4"}} className="font-bold text-4xl">Why choose us</h2>
+                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  pt-6 gap-8 mb-10 h-24 w-full px-10">
+                    {/* Remove class [ h-24 ] when adding a card block */}
+                    <div className="rounded   border-gray-700  border-2 h-24 w-full" >dfgdfgdfgfd</div>
 
-                        <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                            <div>
-                                <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                                    What does success as a UX designer look like and how to get
-                                    there systematically
-                                </h3>
-                            </div>
-                            
-                        </div>
+                    <div className="rounded   border-gray-700  border-2 h-24" ></div>
 
-                        <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                            <div>
-                                <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                                    What does success as a UX designer look like and how to get
-                                    there systematically
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="rounded">
-                    <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                        <div>
-                            <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                                What does success as a UX designer look like and how to get
-                                there systematically
-                            </h3>
-                        </div>
-                        <div>
-                            
-                        </div>
-                    </div>
-
-                    <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                        <div>
-                            <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                                What does success as a UX designer look like and how to get
-                                there systematically
-                            </h3>
-                        </div>
-                        
-                    </div>
-
-                    <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                        <div>
-                            <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                                What does success as a UX designer look like and how to get
-                                there systematically
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="rounded">
-                <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                    <div>
-                        <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                            What does success as a UX designer look like and how to get
-                            there systematically
-                        </h3>
-                    </div>
-                    <div>
-                        
-                    </div>
-                </div>
-
-                <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                    <div>
-                        <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                            What does success as a UX designer look like and how to get
-                            there systematically
-                        </h3>
-                    </div>
-                    
-                </div>
-
-                <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                    <div>
-                        <h3 className="text-gray-800 dark:text-gray-100 leading-7 font-semibold w-11/12">
-                            What does success as a UX designer look like and how to get
-                            there systematically
-                        </h3>
-                    </div>
+                    <div className="rounded   border-gray-700  border-2 h-24 " ></div>
                 </div>
             </div>
-                </div>
-            </div>
-
-            <>
-            <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8 mb-10">
-                {/* Remove class [ h-24 ] when adding a card block */}
-                {/* Remove class [ border-gray-300  dark:border-gray-700 border-dashed border-2 ] to remove dotted border */}
-                <div className="rounded   border-gray-700  border-2 " />
-                
-                <div className="rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24" />
-               
-                <div className="rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24" />
-                
-                
-            </div>
-        </>
         </>
     );
 };
