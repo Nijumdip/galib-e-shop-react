2ac54ff693c444f614214e707ae503143d49ac97
Nijumdip <nijumdip019911@gmail.com>
2023-01-07 13:40:52 +0600

    some style change

--- src/Components/Home/ChooseUs.js
+++ src/Components/Home/ChooseUs.js
@@ -5,9 +5,9 @@ const ChooseUs = () => {
         <>
             <div className="flex flex-col justify-center items-center ">
                 <h2 style={{color:"#00A9E4"}} className="font-bold text-4xl">Why choose us</h2>
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  pt-6 gap-8 mb-10 h-24 w-full px-10">
+                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  pt-6 gap-8 mb-10 h-24 w-full sm:px-20 px-10">
                     {/* Remove class [ h-24 ] when adding a card block */}
-                    <div className="rounded   border-gray-700  border-2 h-24 w-full" >dfgdfgdfgfd</div>
+                    <div className="rounded   border-gray-700  border-2 h-24" >dfgdfgdfgfd</div>
 
                     <div className="rounded   border-gray-700  border-2 h-24" ></div>
 
